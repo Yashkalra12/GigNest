d1fd206d85436efcd752e31ef0475adbe69985e4
Yashkalra12 <yash1578.be21@chitkara.edu.in>
2025-01-23 23:20:49 +0530

    Cors fix

--- backend/server.js
+++ backend/server.js
@@ -22,25 +22,33 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://gig-nest.vercel.app/",
-  "https://gig-nest-backend.vercel.app/api",
-  "https://gig-nest-backend.vercel.app/"
-];
+// const allowedOrigins = [
+  
+//   "http://gig-nest.vercel.app/",
+  
+// ];
+
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//   })
+// );
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
+    origin: 'https://evoting-mern-frontend.vercel.app', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow only GET, POST, PUT, and DELETE requests
+    credentials: true, // Allow credentials (cookies, authorization headers)
   })
 );
+
 
 const connect = async () => {
   try {
